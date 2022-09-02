6bd01b740c4acbc34c3970b7d54f89b31f9873ca
Taavi <taavi.taivere@khk.ee>
2022-09-02 15:16:11 +0300

    Week 1 ex 5

---
+++ Week1/ex5.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Integer days =365;
+        Integer hours = 24;
+        Integer minutes = 60;
+        Integer seconds = 60;
+        Integer secondsInYear = days * hours *minutes * seconds;
+        System.out.println("there are " + secondsInYear + " seconds");
+    }
+}

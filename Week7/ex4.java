c76ed91eb3a9333098e5b2ce42cb7c44c8447140
Taavi <taavi.taivere@khk.ee>
2022-09-17 22:56:06 +0300

    Week 7 - Exercise 7.5: Adding and Translating Words

---
+++ Week7/ex4.java
@@ -0,0 +1,21 @@
+package Week7;
+
+public class ex4 {
+    public static void main(String[] Container) {
+        Container container = new Container(1000);
+        addSuitcasesFullOfBricks(container);
+        System.out.println(container);
+    }
+
+    public static void addSuitcasesFullOfBricks(Container container) {
+        // adding 100 suitcases with one brick in each
+        for (int i = 1; i <= 100; i++) {
+            Suitcase suitcase = new Suitcase(100);
+            Thing brick = new Thing("Brick", i);
+            suitcase.addThing(brick);
+            container.addSuitcase(suitcase);
+        }
+    }
+}
+
+
